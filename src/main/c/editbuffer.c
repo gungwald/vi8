0cc8afc11f39029b1ecf2bc0d11d1cfb6a75fec3
Bill Chatfield <bill_chatfield@yahoo.com>
2020-02-20 17:09:42 -0500

    Added tests

--- src/main/c/editbuffer.c
+++ src/main/c/editbuffer.c
@@ -5,67 +5,98 @@
 
 static FILE *f;
 
-struct EditBuffer *ebInit(struct EditBuffer *b)
+struct EditBuffer* ebInit(struct EditBuffer *b)
 {
-    	b->length = 0;
+	b->length = 0;
+	b->cursorLine = 0;
+	b->cursorLineIndex = 0;
 	return b;
 }
 
-char *ebToString(struct EditBuffer *b, uint16_t index)
+char* ebToString(struct EditBuffer *b, uint16_t index)
 {
 	char *result;
-	size_t requiredSize; 
-	
+	size_t requiredSize;
+
 	requiredSize = b->lines[index].length + 1;
 	result = malloc(sizeof(char) * requiredSize);
-	if (result != NULL) 
-		strncpy(result, b->lines[index].data, requiredSize);
+	if (result != NULL)
+		strcpy(result, b->lines[index].data);
 
 	return result;
 }
 
-struct EditBuffer *ebLoad(struct EditBuffer *b, const char *filename)
+struct EditBuffer* ebLoad(struct EditBuffer *b, const char *filename)
 {
-        f = fopen(filename, "r");
-        if (f != NULL) {
-                ebReadAllLines(b, f);
-                fclose(f);
-        }
+	f = fopen(filename, "r");
+	if (f != NULL)
+	{
+		ebReadAllLines(b, f);
+		fclose(f);
+	}
 	return b;
 }
 
 bool ebIsFull(struct EditBuffer *b)
 {
-        return b->length >= EB_MAX_LEN;
+	return b->length >= EB_MAX_LEN;
 }
 
-struct EditBuffer *ebReadAllLines(struct EditBuffer *b, FILE *f)
+struct EditBuffer* ebReadAllLines(struct EditBuffer *b, FILE *f)
 {
-        ebInit(b);
-        while (!feof(f) && !ferror(f) && !ebIsFull(b))
-                ebReadLine(b, f);
-        return b;
+	ebInit(b);
+	while (!feof(f) && !ferror(f) && !ebIsFull(b))
+		ebReadLine(b, f);
+	return b;
 }
 
-struct EditBuffer *ebReadLine(struct EditBuffer *b, FILE *f)
+struct EditBuffer* ebReadLine(struct EditBuffer *b, FILE *f)
 {
-        int c;
-        uint8_t i = 0;
-        uint8_t lineIndex;
-        char *data;
-        struct Line *line;
+	int c;
+	uint8_t i = 0;
+	uint8_t lineIndex;
+	char *data;
+	struct Line *line;
 
-        lineIndex = b->length;
-        line = &(b->lines[lineIndex]);
-        data = line->data;
-        while (i < EB_MAX_LINE_LEN && (c = getc(f)) != EOF && c != '\r')
-                data[i++] = c;
+	if (b->length < EB_MAX_LEN)
+	{
+		lineIndex = b->length;
+		line = &(b->lines[lineIndex]);
+		data = line->data;
+		while (i < EB_MAX_LINE_LEN && (c = getc(f)) != EOF && c != '\r')
+			data[i++] = c;
 
-        if (ferror(f))
-                perror("Read failed");
+		if (ferror(f))
+			perror("Read failed");
 
-        line->length = i;
-        ++(b->length);
-        return b;
+		line->length = i;
+		++(b->length);
+	}
+	else
+	{
+		fprintf(stderr, "EditBuffer is full");
+	}
+	return b;
+}
+
+struct EditBuffer* ebAppendLineCopy(struct EditBuffer *b, const char *line)
+{
+	struct Line *destLine;
+	char *destData;
+
+	if (b->length < EB_MAX_LEN)
+	{
+		destLine = &(b->lines[b->length]);
+		destData = destLine->data;
+		strncpy(destData, line, EB_MAX_LINE_LEN + 1);
+		destData[EB_MAX_LINE_LEN]; /* Required fail-safe */
+		destLine->length = strlen(destData);
+		++(b->length);
+	}
+	else
+	{
+		fprintf(stderr, "EditBuffer is full");
+	}
+	return b;
 }
 
--- src/test/c/editbuffertest.c
+++ src/test/c/editbuffertest.c
@@ -3,24 +3,35 @@
 #include "editbuffertest.h"
 
 struct EditBuffer b;
+const char *line = "nonsense";
+const char *line2 = "silliness";
 
-int test_ebInit(const char *unusedButRequired)
+int test_ebInit(const char *testName)
 {
-	TINYTEST_EQUAL_MSG(0, ebInit(&b)->length, unusedButRequired);
+	TINYTEST_EQUAL_MSG(0, ebInit(&b)->length, testName);
+}
+
+int test_ebAppendLineCopy(const char *testName)
+{
+	ebAppendLineCopy(ebAppendLineCopy(ebInit(&b), line), line2);
+	TINYTEST_STR_EQUAL_MSG(ebToString(&b, 0), line, testName);
+	TINYTEST_STR_EQUAL_MSG(ebToString(&b, 1), line2, testName);
+}
+
+int test_ebIsFull(const char *testName)
+{
+	TINYTEST_FALSE_MSG(ebIsFull(ebInit(&b)), testName);
+	TINYTEST_FALSE_MSG(ebIsFull(ebAppendLineCopy(&b, line)), testName);
+}
+
+int test_ebToString(const char *testName)
+{
+	TINYTEST_EQUAL_MSG(0, ebInit(&b)->length, testName);
 }
 
 TINYTEST_START_SUITE(EditBuffer);
   TINYTEST_ADD_TEST(test_ebInit,NULL,NULL);
+  TINYTEST_ADD_TEST(test_ebAppendLineCopy,NULL,NULL);
+  TINYTEST_ADD_TEST(test_ebIsFull,NULL,NULL);
+  TINYTEST_ADD_TEST(test_ebToString,NULL,NULL);
 TINYTEST_END_SUITE();
-
-
-/*
-static void test_no_vowels_present()
-{
-    sput_fail_unless(count_vowels("GCC") == 0, "GCC == 0v");
-    sput_fail_unless(count_vowels("BBC") == 0, "BBC == 0v");
-    sput_fail_unless(count_vowels("CNN") == 0, "CNN == 0v");
-    sput_fail_unless(count_vowels("GPS") == 0, "GPS == 0v");
-    sput_fail_unless(count_vowels("Ltd") == 0, "Ltd == 0v");
-}
-*/
