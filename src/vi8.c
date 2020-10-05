f8c90333d432da98acba818c3785b8409e832d5c
Bill Chatfield <bill_chatfield@yahoo.com>
2020-10-05 01:06:13 -0400

    Line to address translation

--- src/vi8.c
+++ src/vi8.c
@@ -1,34 +1,22 @@
-/*
-Table 7-2. Text screen video RAM addresses.
-Base Address
- 0 $400
- 1 $480
- 2 $500
- 3 $580
- 4 $600
- 5 $680
- 6 $700
- 7 $780
- 8 $428
- 9 $4A8
-10 $528
-11 $5A8
-12 $628
-13 $6A8
-14 $728
-15 $7A8
-16 $450
-17 $400
-18 $550
-19 $500
-20 $650
-21 $600
-22 $750
-23 $700
-*/
+#include <stdint.h>
 
 #define TXTPG1 ((char *) 0x0400)
 #define NORMAL(ch) (ch | 0x80)
+
+char *lineAddresses[] = {
+    (char *) 0x400, (char *) 0x480, (char *) 0x500, (char *) 0x580, 
+    (char *) 0x600, (char *) 0x680, (char *) 0x700, (char *) 0x780, 
+    (char *) 0x428, (char *) 0x4A8, (char *) 0x528, (char *) 0x5A8, 
+    (char *) 0x628, (char *) 0x6A8, (char *) 0x728, (char *) 0x7A8, 
+    (char *) 0x450, (char *) 0x400, (char *) 0x550, (char *) 0x500,
+    (char *) 0x650, (char *) 0x600, (char *) 0x750, (char *) 0x700 
+};
+
+uint8_t cursorX = 0;
+uint8_t cursorY = 0;
+
+uint8_t bufferX = 0;
+uint16_t bufferY = 0;
 
 void main(void)
 {
@@ -39,4 +27,9 @@ void main(void)
 	c = NORMAL('A');
 	for (p = TXTPG1; p < endp; ++p)
 		*p = c;
+}
+
+void displayLine(bufferLine, screenLine)
+{
+    char *screenLineAddress = lineAddresses[screenLine];
 }
